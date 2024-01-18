e7467521b6f00590f6fe66d7bac920b5a520ec5a
Alexandre <alexandre.monin03@gmail.com>
2024-01-18 12:22:26 +0100

    docker compose dev locally work

--- app.js
+++ app.js
@@ -6,10 +6,10 @@ let port = process.env.PORT || 3000;
 
 // Configurations de la base de données
 const pool = mariadb.createPool({
-  host: 'localhost',
-  user: 'root',
-  password: 'root',
-  database: 'tp-devops',
+  host: process.env.DB_HOST || 'localhost',
+  user: process.env.DB_USER || 'root',
+  password: process.env.DB_PASSWORD || 'root',
+  database: process.env.DB_DATABASE || 'tp-devops',
   connectionLimit: 5
 });
 
